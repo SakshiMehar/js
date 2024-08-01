c2a45c0259e67304e3f5f3750f1eee22315aa802
SakshiMehar <162415329+SakshiMehar@users.noreply.github.com>
2024-08-01 14:49:52 +0000

    config

---
+++ 02_Arrays/01_Introduction.js
@@ -0,0 +1,13 @@
+const myarr =[1,2,3,4,5,6];
+/*myarr.push(7);
+myarr.pop(); //don't take any argument and delete elements from back
+myarr.unshift(0);// insert item at the beginning 
+myarr.shift(); // delete item from beginning 
+console.log(myarr);
+console.log(myarr.includes(8));//check whether the element are in array or not
+console.log("index of 5 is :",myarr.indexOf(5));// return the index of the element
+*/
+const newArr = myarr.join();//convert array into string
+console.log(myarr);
+console.log(newArr);
+console.log("type of newarr :-",typeof(newArr));
